215c6602cb0b65fa3ccd6aa6480e608e678d5e67
logan-r <kai.ralston@gmail.com>
2018-12-20 16:25:19 -0500

    Implement averaging of one-to-many mapping

--- solution.py
+++ solution.py
@@ -1,17 +1,17 @@
 import csv
 
 """
-Open a .csv file with a given file name (must have a heading row that contains
-the values "purpose" and "int_rate") and create a dictonary that maps each
-unique purpose to a list containing the interest rates of every row that has
-that specified purpose.
+Open a .csv file with a given file name (must have a heading row that
+contains the values "purpose" and "int_rate") and create a dictonary that
+maps each unique purpose to a list containing the interest rates of every
+row that has that specified purpose.
 @param fileName {String}  the name of the csv file to process
-@return  a dictionary mapping purposes to their matching interest rate
-         e.g.:
-         {
-             'credit card': [12.3, 10.5],
-             'car': [4.2, 3.5, 6.2]
-         }
+@returns  a dictionary mapping purposes to their matching interest rate
+          e.g.:
+          {
+              'credit card': [12.3, 10.5],
+              'car': [4.2, 3.5, 6.2]
+          }
 """
 def createPurposeToRateMapFromCsv(filename):
     # Create a dictionary to hold the one-to-many mapping of purposes to
@@ -47,5 +47,33 @@ def createPurposeToRateMapFromCsv(filename):
         # Handle file errors
         print('Invalid file')
 
-d = createPurposeToRateMapFromCsv('data.csv')
-print(d)
+
+"""
+Takes a dictionary of miscellaneous keys mapped to lists of numbers
+and returns a dictionary of the same keys mapped to the average value
+of the list of numbers they were previously mapped to.
+@param ogMap {Dict}  a dictionary with keys of any type mapped to lists
+                     of numbers (e.g. floats, ints - any object that can
+                     be summed and divided)
+@returns {Dict}  a dictionary with keys of any type mapped to a singular
+                 number: the average of the list from the inputted
+                 dictionary
+"""
+def averageOfOneToManyMapping(ogMap):
+    # Create a dictionary to hold return value of this function: the
+    # original keys mapped to the averaged value
+    averagedMap = {}
+
+    # Iterate through every key in the original mapping and add the
+    # averaged equivalent to the averageMap
+    for key in ogMap:
+        averagedMap[key] = sum(ogMap[key]) / float(len(ogMap[key]))
+
+    # Return the mapping that now contains the averaged values
+    return averagedMap
+
+if __name__ == "__main__":
+    # Calculate mapping of purposes to average interest rates from file
+    purposesToAvgRateMap = averageOfOneToManyMapping(
+        createPurposeToRateMapFromCsv('data.csv')
+    )
